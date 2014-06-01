7192b30d9fc6d736b161c82623dd6347cf0036ea
binling <binling@gmail.com>
2014-06-01 19:40:07 +0100

    my spring tutorial at github

---
+++ src/springapp/web/HelloController.java
@@ -0,0 +1,22 @@
+package springapp.web;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloController {
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse reponse)
+		throws ServletException, IOException {
+		logger.info("Return Hello View");
+		return new ModelAndView("hello.jsp");
+	}
+}
